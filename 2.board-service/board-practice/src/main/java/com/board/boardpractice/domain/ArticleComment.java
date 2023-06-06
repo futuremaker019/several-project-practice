f82392c37cef083f7e7d4d241923a64d622b7329
Goohyun Jung <futuremaker019@gmail.com>
2023-06-06 23:20:47 +0900

    #8 - 03.데이터베이스 접근 로직 테스즈 정의 3까지의 강의코드 정리

--- 2.board-service/board-practice/src/main/java/com/board/boardpractice/domain/ArticleComment.java
+++ 2.board-service/board-practice/src/main/java/com/board/boardpractice/domain/ArticleComment.java
@@ -1,14 +1,72 @@
 package com.board.boardpractice.domain;
 
-import java.time.LocalDateTime;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdBy"),
+        @Index(columnList = "createdAt")
+})
+@Entity
 public class ArticleComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    /**
+     * optional 이 아닌 필수값이라는 설정
+     * cascade는 none이 기본값 (댓글은 게시글과 독립적으로 행동하므로 적용하지 않음)
+     */
+    @Setter
+    @ManyToOne(optional = false)
     private Article article;    // 개시글 Id
+    @Setter
+    @Column(nullable = false, length = 500)
     private String content;     // 본문
 
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt;
-    private String createBy;
+    @CreatedBy
+    @Column(nullable = false, length = 100)
+    private String createdBy;
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt;
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy;
+
+    private ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    public static ArticleComment of(Article article, String content) {
+        return new ArticleComment(article, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment that)) return false;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
---
+++ 2.board-service/board-practice/src/test/java/com/board/boardpractice/repository/JpaRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.board.boardpractice.repository;
+
+import com.board.boardpractice.config.JpaConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(
+            @Autowired ArticleRepository articleRepository,
+            @Autowired ArticleCommentRepository articleCommentRepository
+    ) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("select test")
+    @Test
+    void givenTestData_whenSelecting_thenWorkfind() {
+
+    }
+}
