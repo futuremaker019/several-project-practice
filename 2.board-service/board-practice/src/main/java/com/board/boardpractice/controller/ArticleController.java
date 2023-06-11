83cc212ebe5070ae6bde1c940e6f1bf498b6218c
Goohyun Jung <futuremaker019@gmail.com>
2023-06-11 17:30:14 +0900

    #17 - 게시글 관련 뷰 테스트 작성

---
+++ 2.board-service/board-practice/src/main/java/com/board/boardpractice/controller/ArticleController.java
@@ -0,0 +1,11 @@
+package com.board.boardpractice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/article")
+public class ArticleController {
+
+
+}
