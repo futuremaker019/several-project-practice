4b30297f4cd076549f888cc249de128acd9782ff
futuremaker019 <futuremaker019@gmail.com>
2023-06-12 18:03:31 +0900

    #17 - thymleaf 템플릿 반영

--- 2.board-service/board-practice/src/test/java/com/board/boardpractice/controller/ArticleControllerTest.java
+++ 2.board-service/board-practice/src/test/java/com/board/boardpractice/controller/ArticleControllerTest.java
@@ -37,7 +37,7 @@ class ArticleControllerTest {
     void givenNothing_whenRequestingArticleView_thenReturnArticleView() throws Exception {
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -49,7 +49,7 @@ class ArticleControllerTest {
     void givenNothing_whenRequestingArticleSearchView_thenReturnArticleSearchView() throws Exception {
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search"));
     }
 
@@ -59,7 +59,7 @@ class ArticleControllerTest {
     void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnArticleHashtagSearchView() throws Exception {
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search-hashtag"));
     }
 }
