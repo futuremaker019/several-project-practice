18a77460a641a5e072aa68bfb262ff118e647199
Goohyun Jung <futuremaker019@gmail.com>
2023-06-20 22:58:00 +0900

    #19 - 05.게시판 페이지 기능 구현2 (ArticleController 기능구현)

--- 2.board-service/board-practice/src/main/java/com/board/boardpractice/controller/ArticleController.java
+++ 2.board-service/board-practice/src/main/java/com/board/boardpractice/controller/ArticleController.java
@@ -1,30 +1,46 @@
 package com.board.boardpractice.controller;
 
-import com.board.boardpractice.domain.Article;
+import com.board.boardpractice.domain.type.SearchType;
+import com.board.boardpractice.response.ArticleResponse;
+import com.board.boardpractice.response.ArticleWithCommentsResponse;
+import com.board.boardpractice.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/articles")
 public class ArticleController {
 
+    private final ArticleService articleService;
+
     @GetMapping
-    public String articles(ModelMap map) {
-        map.addAttribute("articles", List.of());
+    public String articles(
+            @RequestParam(required = false) SearchType searchType,
+            @RequestParam(required = false) String searchValue,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            ModelMap map
+    ) {
+        map.addAttribute("articles",
+                articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from)
+        );
 
         return "articles/index";
     }
 
     @GetMapping("/{articleId}")
     public String articles(@PathVariable Long articleId, ModelMap map) {
-        // TODO : 구현시 실제 데이터 필요
-        map.addAttribute("article", "article");
-        map.addAttribute("articleComments", List.of());
+        ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
+        map.addAttribute("article", article);
+        map.addAttribute("articleComments", article.articleCommentsResponses());
 
         return "articles/detail";
     }
