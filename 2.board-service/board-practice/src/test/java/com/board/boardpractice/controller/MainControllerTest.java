a2d524dbf9c7d2a3f977a29582b5b31e11c55d7e
Goohyun Jung <futuremaker019@gmail.com>
2023-07-02 23:10:44 +0900

    #34 - 테스트케이스 실패로 인한 코드 수정

--- 2.board-service/board-practice/src/test/java/com/board/boardpractice/controller/MainControllerTest.java
+++ 2.board-service/board-practice/src/test/java/com/board/boardpractice/controller/MainControllerTest.java
@@ -6,10 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
